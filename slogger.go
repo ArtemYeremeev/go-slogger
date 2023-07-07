79b8d507e1e94df2fb1d1cba066de249146d98d0
eremeev_as <eremeev_as@apteka-aprel.ru>
2023-07-07 15:51:19 +0300

    init

---
+++ slogger.go
@@ -0,0 +1,75 @@
+package slogger
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/fatih/color"
+	"golang.org/x/exp/slog"
+)
+
+const (
+	Debug  = slog.Level(-8)
+	Notice = slog.Level(-4)
+	Info   = slog.Level(0)
+	Warn   = slog.Level(4)
+	Fatal  = slog.Level(8)
+)
+
+// Levels описывает наименования произвольные уровни логирования
+var Levels = map[slog.Leveler]string{
+	Debug:  "[DEBUG]",
+	Notice: "[NOTICE]",
+	Info:   "[INFO]",
+	Warn:   "[WARNING]",
+	Fatal:  "[FATAL]",
+}
+
+// ColorLevels описывает цветовую схему произвольных уровней логирования
+var ColorLevels = map[slog.Leveler]func(format string, a ...interface{}) string {
+	Debug: color.BlueString,
+	Notice: color.YellowString,
+	Info: color.GreenString,
+	Warn: color.RedString,
+	Fatal: color.MagentaString,
+}
+
+// Lvler регулирует уровень логирования
+var Lvler slog.LevelVar
+
+// Make создает структурированный лог и заменяет лог по умолчанию
+func Make(logPath string, defaultLevel slog.Level) {
+	var (
+		w io.Writer
+		h slog.Handler
+	)
+	if logPath != "" {
+		_, err := os.Stat(logPath)
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(filepath.Dir(logPath), os.ModePerm); err != nil {
+				os.Stderr.WriteString("не удалось создать директорию для сохранения файла лога")
+			}
+		
+			err = ioutil.WriteFile(logPath, nil, os.ModePerm)
+			if err != nil {
+				os.Stderr.WriteString("не удалось записать файл лога")
+			}
+		}
+
+		w, err = os.OpenFile(logPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, os.ModePerm)
+		if err != nil {
+			w = os.Stdout
+		}
+
+		h = wrapLogParams().NewJSONHandler(w)
+	} else {
+		h = wrapLogParams().NewTextHandler(os.Stdout)
+	}
+
+	// Установка уровня логирования
+	Lvler.Set(defaultLevel)
+
+	slog.SetDefault(slog.New(h))
+}
---
+++ write.go
@@ -0,0 +1,75 @@
+package slogger
+
+import (
+	"context"
+	"time"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+	"golang.org/x/exp/slog"
+)
+
+// LogData описывает структуру единицы логирования
+type LogData struct {
+	Level  slog.Level
+	Attrs  []slog.Attr
+}
+
+// On создает единицу логирования с уровнем lvl
+func On(lvl slog.Level) *LogData {
+	return &LogData{
+		Level: lvl,
+	}
+}
+
+// Print добавляет данные в файл лога
+func (ld *LogData) Print(msg string, args ...interface{}) {
+	msg = strings.TrimSuffix(color.CyanString(fmt.Sprintln(append([]interface{}{msg}, args...)...)), "\n")
+
+	switch ld.Level {
+	case Debug:
+		slog.Log(context.Background(), ld.Level, msg, ld.Attrs)
+	case Notice:
+		slog.Log(context.Background(), ld.Level, msg, ld.Attrs)
+	case Info:
+		slog.Info(msg, ld.Attrs)
+	case Warn:
+		slog.Warn(msg, ld.Attrs)
+	case Fatal:
+		os.Exit(1)
+	default:
+		slog.Info("Undefined log level - " + ld.Level.String())
+	}
+}
+
+// wrapLogParams инициализирует параметры логгера
+func wrapLogParams() slog.HandlerOptions {
+	return slog.HandlerOptions{
+		AddSource: false,
+		Level:     &Lvler,
+		ReplaceAttr: func(gps []string, attr slog.Attr) slog.Attr {
+			if attr.Key == slog.TimeKey { // Форматирование временной метки
+				attr.Value = slog.StringValue(fmt.Sprint(time.Now().Format("2006-01-02 15:04:05")))
+			}
+
+			if attr.Key == slog.LevelKey {
+				// Вывод наименования уровня логирования
+				level := attr.Value.Any().(slog.Level)
+				levelLabel, exists := Levels[level]
+				if !exists {
+					levelLabel = level.String()
+				}
+
+				// Замена цвета уровня логирования
+				f := ColorLevels[level]
+				levelLabel = f(levelLabel)
+
+				attr.Value = slog.StringValue(levelLabel)
+			}
+
+			return attr
+		},
+	}
+}
